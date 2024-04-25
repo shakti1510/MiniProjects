1c5bafd5eef98bbe497e4078ede9a8623288291d
shakti1510 <shashankmnshakti95288@gmail.com>
2024-04-25 22:10:16 +0530

    4th project

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,7 +1,7 @@
-const open=document.getElementById('open')
-const close=document.getElementById('close')
-const container=document.querySelector('.container')
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
-open.addEventListener('submit',()=>container.classList('show-nav'))
+open.addEventListener("click", () => container.classList.add("show-nav"));
 
-close.addEventListener('submit',()=>container.remove('show-nav'))
+close.addEventListener("click", () => container.classList.remove("show-nav"));
